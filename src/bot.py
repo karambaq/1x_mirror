bbf02f3bd2032e5837e21f46aaeb9a75d1df9d21
karambaq <0maximq@gmail.com>
2018-06-07 14:17:58 +0300

    Counting requests from users

--- src/bot.py
+++ src/bot.py
@@ -5,8 +5,11 @@ from telegram.ext import CommandHandler
 
 from get_mirror import get_current_mirror
 
+REQUESTS_COUNT = 0
 
 def start(bot, update):
+    REQUESTS_COUNT += 1
+    print(REQUESTS_COUNT)
     bot.sendMessage(chat_id=update.message.chat_id, text=f"{get_current_mirror()}")
 
 
