e4356c47afde944a267673fe985c2413b3feac49
karambaq <0maximq@gmail.com>
2018-06-07 14:27:04 +0300

    Counting requests from users

--- src/bot.py
+++ src/bot.py
@@ -7,14 +7,12 @@ from get_mirror import get_current_mirror
 
 
 def start(bot, update):
-    REQUESTS_COUNT += 1
     print(REQUESTS_COUNT)
     bot.sendMessage(chat_id=update.message.chat_id, text=f"{get_current_mirror()}")
 
 
 def polling():
-    REQUESTS_COUNT = 0
-    updater = Updater(token='607298602:AAHmHXlRkNJ3rT2dPiij3Ah2hAy0gnu7M6w')
+    updater = Updater(token=os.environ.get('TOKEN'))
     start_handler = CommandHandler('start', start) 
     updater.dispatcher.add_handler(start_handler) 
     updater.start_polling() 
